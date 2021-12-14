f43a2532f63a341c630fd74e3f7163d7e7af5491
bowwowden <william.f.bowden@gmail.com>
2021-12-13 19:15:29 -0700

    Added a canvas page that needs some love. Also fixed the hyperlink colors on the main page.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This is a website that has been published to GitHub pages that was created during the SoDA Bootcamp.
 
+## 
+
+npm start uses the http-server package.
+If changes to updates not reflected in browser, hard reload necessary because memory may be in cache.
---
+++ graph.js
@@ -0,0 +1,90 @@
+
+var treeData = [
+  {
+    "name": "A",
+    "parent": "null",
+    "children": [
+      {
+        "name": "B",
+        "parent": "A",
+        "children": [
+          {
+            "name": "D",
+            "parent": "B"
+          },
+          {
+            "name": "E",
+            "parent": "B"
+          }
+        ]
+      },
+      {
+        "name": "C",
+        "parent": "A"
+      }
+    ]
+  }
+];
+
+// ************** Generate the tree diagram	 *****************
+var margin = {top: 40, right: 20, bottom: 20, left: 20},
+	width = 400 - margin.right - margin.left,
+	height = 400 - margin.top - margin.bottom;
+	
+var i = 0;
+
+var tree = d3.layout.tree()
+	.size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+	.projection(function(d) { return [d.x, d.y]; });
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.right + margin.left)
+	.attr("height", height + margin.top + margin.bottom)
+  .append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+root = treeData[0];
+  
+update(root);
+
+function update(source) {
+
+  // Compute the new tree layout.
+  var nodes = tree.nodes(root).reverse(),
+	  links = tree.links(nodes);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d) { d.y = d.depth * 100; });
+
+  // Declare the nodes…
+  var node = svg.selectAll("g.node")
+	  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+  // Enter the nodes.
+  var nodeEnter = node.enter().append("g")
+	  .attr("class", "node")
+	  .attr("transform", function(d) { 
+		  return "translate(" + d.x + "," + d.y + ")"; });
+
+  nodeEnter.append("circle")
+	  .attr("r", 20)
+	  .style("fill", "#fff");
+
+  nodeEnter.append("text")
+	  .attr("dy", ".35em")
+	  .attr("text-anchor", "middle")
+	  .text(function(d) { return d.name; })
+	  .style("fill-opacity", 1);
+
+  // Declare the links…
+  var link = svg.selectAll("path.link")
+	  .data(links, function(d) { return d.target.id; });
+
+  // Enter the links.
+  link.enter().insert("path", "g")
+	  .attr("class", "link")
+	  .attr("d", diagonal);
+
+}
